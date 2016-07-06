de218704750e3115de84a7f03c338b4066526f97
陈丰尧 <734481463@qq.com>
2016-07-06 16:27:42 +0800

    LocalPlayBack写到了 播放 写的略微崩溃，要搞懂一个方法，就会产生2个没讲的类，要搞懂这两个没见过的类就会产生4个没见过的类。。。

--- MusicDemo/app/src/main/java/com/lanou/chenfengyao/musicdemo/model/MusicProvider.java
+++ MusicDemo/app/src/main/java/com/lanou/chenfengyao/musicdemo/model/MusicProvider.java
@@ -1,9 +1,16 @@
 package com.lanou.chenfengyao.musicdemo.model;
 
+import android.support.v4.media.MediaMetadataCompat;
+
 /**
  * Created by ChenFengYao on 16/7/5.
  * 简单的音乐曲目数据提供者。
  * 来提供音乐数据的类。
  */
 public class MusicProvider {
+
+    //TODO MediaMetadataCompat 是什么
+    public MediaMetadataCompat getMusic(String musicId){
+        return null;
+    }
 }
