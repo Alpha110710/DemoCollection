384db16d8a7a1b15c7c001d3370f78ad380ecc21
陈丰尧 <734481463@qq.com>
2016-07-07 22:51:49 +0800

    no message

--- MusicDemo/app/src/main/java/com/lanou/chenfengyao/musicdemo/model/MusicProvider.java
+++ MusicDemo/app/src/main/java/com/lanou/chenfengyao/musicdemo/model/MusicProvider.java
@@ -1,26 +1,7 @@
 package com.lanou.chenfengyao.musicdemo.model;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.v4.media.MediaBrowserCompat;
-import android.support.v4.media.MediaDescriptionCompat;
-import android.support.v4.media.MediaMetadataCompat;
-import android.util.Log;
-
-import com.lanou.chenfengyao.musicdemo.R;
-import com.lanou.chenfengyao.musicdemo.utils.MediaIDHelper;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import static com.lanou.chenfengyao.musicdemo.utils.MediaIDHelper.*;
 
 /**
  * Created by ChenFengYao on 16/7/5.
@@ -28,303 +9,28 @@ import static com.lanou.chenfengyao.musicdemo.utils.MediaIDHelper.*;
  * 来提供音乐数据的类。
  */
 public class MusicProvider {
-
-    private MusicProviderSource mSource;
-
-
-
-    //分类缓存的音乐曲目数据
-    //ConcurrentMap 线程安全的Map
-    private ConcurrentMap<String, List<MediaMetadataCompat>> mMusicListByGenre;
-    private ConcurrentMap<String, MutableMediaMetadata> mMusicListById;
-
-    private Set<String> mFavoriteTracks;
-
-    //代表初始化的状态 未初始化,初始化中,和初始化结束
-    enum State {
-        NON_INITIALIZED, INITIALIZING, INITIALIZED
-    }
-
-    //标记当前的初始化状态,默认是未初始化
-    private volatile State mCurrentState = State.NON_INITIALIZED;
-
-    public interface Callback {
-        void onMusicCatalogReady(boolean success);
-    }
+    private List<MusicBean> musicBeanList;//播放列表
 
     public MusicProvider() {
-        //TODO 这里是加载的远端的数据资源,改动这里应该可以改变加载的数据
-        this(new RemoteJSONSource());
+        getMusicBeanList();
     }
 
-    public MusicProvider(MusicProviderSource source) {
-        mSource = source;
-        mMusicListByGenre = new ConcurrentHashMap<>();
-        mMusicListById = new ConcurrentHashMap<>();
-        //将set和map建立联系,set里存放的map的key,改变set map也会跟着改变
-        mFavoriteTracks = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+    //获得歌曲列表
+    public void getMusicBeanList(){
+        musicBeanList = new ArrayList<>();
     }
 
-    /**
-     * 返回所有的歌曲风格
-     *
-     * @return genres
-     */
-    public Iterable<String> getGenres() {
-        if (mCurrentState != State.INITIALIZED) {
-            return Collections.emptyList();
+    //设置歌曲列表
+    public void setMusicBeanList(){
+
+    }
+
+    //通过索引来确定播放的歌曲
+    public MusicBean getMusicFromIndex(int index){
+        if(index>=0 && index < musicBeanList.size()){
+            return musicBeanList.get(index);
         }
-        return mMusicListByGenre.keySet();
+        return null;
     }
-
-    /**
-     * 返回所有曲目的集合(顺序是随机的)
-     */
-    public Iterable<MediaMetadataCompat> getShuffledMusic() {
-        if (mCurrentState != State.INITIALIZED) {
-            return Collections.emptyList();
-        }
-        List<MediaMetadataCompat> shuffled = new ArrayList<>(mMusicListById.size());
-        for (MutableMediaMetadata mutableMetadata : mMusicListById.values()) {
-            shuffled.add(mutableMetadata.metadata);
-        }
-        //打乱顺序
-        Collections.shuffle(shuffled);
-        return shuffled;
-    }
-
-    /**
-     * 按照给定的风格返回歌曲信息
-     */
-    public Iterable<MediaMetadataCompat> getMusicsByGenre(String genre) {
-        if (mCurrentState != State.INITIALIZED || !mMusicListByGenre.containsKey(genre)) {
-            return Collections.emptyList();
-        }
-        return mMusicListByGenre.get(genre);
-    }
-
-
-    /**
-     * 通过歌曲的标题来搜索曲目
-     */
-    public Iterable<MediaMetadataCompat> searchMusicBySongTitle(String query) {
-        return searchMusic(MediaMetadataCompat.METADATA_KEY_TITLE, query);
-    }
-
-    public Iterable<MediaMetadataCompat> searchMusicByAlbum(String query) {
-        return searchMusic(MediaMetadataCompat.METADATA_KEY_ALBUM, query);
-    }
-
-    public Iterable<MediaMetadataCompat> searchMusicByArtist(String query) {
-        return searchMusic(MediaMetadataCompat.METADATA_KEY_ARTIST, query);
-    }
-
-    //通过歌曲的指定信息来搜索,例如通过标题,歌手等
-    Iterable<MediaMetadataCompat> searchMusic(String metadataField, String query) {
-        if (mCurrentState != State.INITIALIZED) {
-            return Collections.emptyList();
-        }
-        ArrayList<MediaMetadataCompat> result = new ArrayList<>();
-        query = query.toLowerCase(Locale.US);
-        for (MutableMediaMetadata track : mMusicListById.values()) {
-            if (track.metadata.getString(metadataField).toLowerCase(Locale.US)
-                    .contains(query)) {
-                result.add(track.metadata);
-            }
-        }
-        return result;
-    }
-
-    /**
-     * 根据给定的id来返回歌曲信息
-     * MediaMetadataCompat 包含歌曲信息的数据类,有例如歌手,标题的信息
-     *
-     * @param musicId
-     */
-    public MediaMetadataCompat getMusic(String musicId) {
-        return mMusicListById.containsKey(musicId) ?
-                mMusicListById.get(musicId).metadata : null;
-    }
-
-    //更新歌曲信息
-    public synchronized void updateMusicArt(String musicId, Bitmap albumArt, Bitmap icon) {
-        MediaMetadataCompat metadata = getMusic(musicId);
-        metadata = new MediaMetadataCompat.Builder(metadata)
-                .putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, albumArt)
-                //设施icon是必须的
-                .putBitmap(MediaMetadataCompat.METADATA_KEY_DISPLAY_ICON, icon)
-                .build();
-
-        MutableMediaMetadata mutableMetadata = mMusicListById.get(musicId);
-        if (mutableMetadata == null) {
-            throw new IllegalStateException("Unexpected error: Inconsistent data structures in " +
-                    "MusicProvider(没有找到指定ID的歌曲,ID错误)");
-        }
-        mutableMetadata.metadata = metadata;
-    }
-
-    //标记喜欢/取消喜欢
-    public void setFavorite(String musicId, boolean favorite) {
-        if (favorite) {
-            mFavoriteTracks.add(musicId);
-        } else {
-            mFavoriteTracks.remove(musicId);
-        }
-    }
-
-    //是否初始化完成
-    public boolean isInitialized() {
-        return mCurrentState == State.INITIALIZED;
-    }
-
-    //查询某一首歌是否是喜欢的
-    public boolean isFavorite(String musicId) {
-        return mFavoriteTracks.contains(musicId);
-    }
-
-    /**
-     * 异步的获取播放列表
-     */
-    public void retrieveMediaAsync(final Callback callback) {
-        if (mCurrentState == State.INITIALIZED) {
-            //如果初始化结束了,就不需要再去获取了
-            if (callback != null) {
-                //回调,Music准备好了
-                callback.onMusicCatalogReady(true);
-            }
-            return;
-        }
-
-        //开启一个异步任务 来获获取媒体信息
-        new AsyncTask<Void, Void, State>() {
-            @Override
-            protected State doInBackground(Void... params) {
-                retrieveMedia();//获取媒体信息
-                return mCurrentState;
-            }
-
-            @Override
-            protected void onPostExecute(State current) {
-                if (callback != null) {
-                    //媒体信息获取完成与否
-                    callback.onMusicCatalogReady(current == State.INITIALIZED);
-                }
-            }
-        }.execute();
-    }
-
-    //创建歌曲的类型
-    private synchronized void buildListsByGenre() {
-        ConcurrentMap<String, List<MediaMetadataCompat>> newMusicListByGenre = new ConcurrentHashMap<>();
-
-        for (MutableMediaMetadata m : mMusicListById.values()) {
-            String genre = m.metadata.getString(MediaMetadataCompat.METADATA_KEY_GENRE);
-            List<MediaMetadataCompat> list = newMusicListByGenre.get(genre);
-            if (list == null) {
-                list = new ArrayList<>();
-                newMusicListByGenre.put(genre, list);
-            }
-            list.add(m.metadata);
-        }
-        mMusicListByGenre = newMusicListByGenre;
-    }
-
-    //获取媒体信息
-    private synchronized void retrieveMedia() {
-        try {
-            if (mCurrentState == State.NON_INITIALIZED) {
-                //改变状态为 正在初始化
-                mCurrentState = State.INITIALIZING;
-
-                Iterator<MediaMetadataCompat> tracks = mSource.iterator();
-                while (tracks.hasNext()) {
-                    MediaMetadataCompat item = tracks.next();
-                    String musicId = item.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID);
-                    //把获取的歌曲都放进去
-                    mMusicListById.put(musicId, new MutableMediaMetadata(musicId, item));
-                }
-                buildListsByGenre();
-                //初始化完成
-                mCurrentState = State.INITIALIZED;
-            }
-        } finally {
-            if (mCurrentState != State.INITIALIZED) {
-                //如果因为某些原因,没有让状态变为初始化完成,就再将状态变为还没有初始化
-                mCurrentState = State.NON_INITIALIZED;
-            }
-        }
-    }
-
-    //TODO 这个方法实在是看不动了
-    public List<MediaBrowserCompat.MediaItem> getChildren(String mediaId, Resources resources) {
-        List<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
-
-        if (!MediaIDHelper.isBrowseable(mediaId)) {
-            return mediaItems;
-        }
-
-        if (MEDIA_ID_ROOT.equals(mediaId)) {
-            mediaItems.add(createBrowsableMediaItemForRoot(resources));
-
-        } else if (MEDIA_ID_MUSICS_BY_GENRE.equals(mediaId)) {
-            for (String genre : getGenres()) {
-                mediaItems.add(createBrowsableMediaItemForGenre(genre, resources));
-            }
-
-        } else if (mediaId.startsWith(MEDIA_ID_MUSICS_BY_GENRE)) {
-            String genre = MediaIDHelper.getHierarchy(mediaId)[1];
-            for (MediaMetadataCompat metadata : getMusicsByGenre(genre)) {
-                mediaItems.add(createMediaItem(metadata));
-            }
-
-        } else {
-            Log.w("MusicProvider", "Skipping unmatched mediaId: "+mediaId);
-        }
-        return mediaItems;
-    }
-
-    private MediaBrowserCompat.MediaItem createBrowsableMediaItemForRoot(Resources resources) {
-        MediaDescriptionCompat description = new MediaDescriptionCompat.Builder()
-                .setMediaId(MEDIA_ID_MUSICS_BY_GENRE)
-                .setTitle(resources.getString(R.string.browse_genres))
-                .setSubtitle(resources.getString(R.string.browse_genre_subtitle))
-                .setIconUri(Uri.parse("android.resource://" +
-                        "com.example.android.uamp/drawable/ic_by_genre"))
-                .build();
-        return new MediaBrowserCompat.MediaItem(description,
-                MediaBrowserCompat.MediaItem.FLAG_BROWSABLE);
-    }
-
-    private MediaBrowserCompat.MediaItem createBrowsableMediaItemForGenre(String genre,
-                                                                          Resources resources) {
-        MediaDescriptionCompat description = new MediaDescriptionCompat.Builder()
-                .setMediaId(createMediaID(null, MEDIA_ID_MUSICS_BY_GENRE, genre))
-                .setTitle(genre)
-                .setSubtitle(resources.getString(
-                        R.string.browse_musics_by_genre_subtitle, genre))
-                .build();
-        return new MediaBrowserCompat.MediaItem(description,
-                MediaBrowserCompat.MediaItem.FLAG_BROWSABLE);
-    }
-
-    private MediaBrowserCompat.MediaItem createMediaItem(MediaMetadataCompat metadata) {
-        // Since mediaMetadata fields are immutable, we need to create a copy, so we
-        // can set a hierarchy-aware mediaID. We will need to know the media hierarchy
-        // when we get a onPlayFromMusicID call, so we can create the proper queue based
-        // on where the music was selected from (by artist, by genre, random, etc)
-        String genre = metadata.getString(MediaMetadataCompat.METADATA_KEY_GENRE);
-        String hierarchyAwareMediaID = MediaIDHelper.createMediaID(
-                metadata.getDescription().getMediaId(), MEDIA_ID_MUSICS_BY_GENRE, genre);
-        MediaMetadataCompat copy = new MediaMetadataCompat.Builder(metadata)
-                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, hierarchyAwareMediaID)
-                .build();
-        return new MediaBrowserCompat.MediaItem(copy.getDescription(),
-                MediaBrowserCompat.MediaItem.FLAG_PLAYABLE);
-
-    }
-
-
-
-
 
 }
